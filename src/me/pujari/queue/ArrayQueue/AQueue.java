fc6533fd367c24032d254858ca9fecede0fd8fd3
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-08 21:48:29 +0530

    Stage 2 - Circulate Q Started

--- src/me/pujari/queue/ArrayQueue/AQueue.java
+++ src/me/pujari/queue/ArrayQueue/AQueue.java
@@ -6,39 +6,71 @@ package me.pujari.queue.ArrayQueue;
  */
 public class AQueue {
     private int [] queue;
-    private int queuePointer;
+    private int head;
+    private int tail;
 
     public AQueue(){
         queue = new int[10];
-        queuePointer = 0;
+        head  = tail = 0;
     }
     public void enqueue(int v){
-        if(queuePointer > 9 ){
+        if((head == 0 && tail > queue.length) || head > 0 && tail+1 == head){
             System.out.println("Queue is Full");
         }else {
-            queue[queuePointer++] = v;
+            queue[tail++] = v;
         }
     }
 
     public int dequeue(){
         int temp = -1;
-        if(queuePointer < 0){
+        if(tail < 0){
             System.out.println("Queue is Empty");
         }else {
             temp = queue[0];
-            for(int i =0; i < queuePointer - 1;i++){
+            for(int i =0; i < tail - 1;i++){
                 queue[i] = queue[i+1];
             }
-            queuePointer--;
+            tail--;
         }
         return temp;
     }
 
     public void print(){
         System.out.println("------------------------X-----------------------");
-        for(int i = 0 ; i < queuePointer ; i++){
+        for(int i = 0 ; i < tail ; i++){
             System.out.println("Queue["+i+"] = "+queue[i]);
         }
         System.out.println("------------------------X-----------------------");
+    }
+
+    public int incrementHead(){
+        if((tail + 1) >= queue.length && head > 0){
+            tail = 0 ;
+            if(!((tail + 1) >= head)){
+
+            }
+        }else{
+
+        }
+    }
+
+    public int incrementTail(){
+        if((tail + 1) >= queue.length && head > 0){
+            tail = 0 ;
+            if(!((tail + 1) >= head)){
+
+            }
+        }else{
+
+        }
+        return 0;
+    }
+
+    public void restHead(){
+        head = 0;
+    }
+
+    public void restTail(){
+        tail = 0;
     }
 }
